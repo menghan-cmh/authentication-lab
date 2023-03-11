9f62f91398e94356c6d3ae15511dd9c96736f4a9
menghan-cmh <meghanchen2001@gmail.com>
2023-03-10 17:23:21 -0700

    update

--- client/admin.py
+++ client/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from rest_framework.authtoken.admin import TokenAdmin
+from .models import Snippet
 
 # Register your models here.
+admin.site.register(Snippet)
+TokenAdmin.raw_id_fields = ('user',)
